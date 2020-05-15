cadfa53b9fe6b97b99f62db2e92482f47ac91f84
stardiviner <numbchild@gmail.com>
2020-05-15 11:46:11 +0800

    initialize source code

---
+++ comment-annotate.el
@@ -0,0 +1,19 @@
+;;; comment-annotate.el --- annotate code with beautiful comments -*- lexical-binding: t; -*-
+
+;;; Time-stamp: <2020-05-15 11:46:09 stardiviner>
+
+;;; Commentary:
+
+
+
+;;; Code:
+
+
+
+
+
+
+
+(provide 'comment-annotate)
+
+;;; comment-annotate.el ends here
